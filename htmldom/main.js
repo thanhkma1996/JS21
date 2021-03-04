52147c5f0ff2336301fcd32af93809bd0a9b3067
thanhkma1996 <soldiersoociu@gmail.com>
2021-03-04 23:00:19 +0700

    4/3 DOM event

--- htmldom/main.js
+++ htmldom/main.js
@@ -33,3 +33,39 @@ headingElemment.setAttribute('class','testabc');
 
 console.log(headingElemment.innerText);
 console.log(headingElemment.textContent);
+
+// InnerHTML propertype
+var boxHTML = document.querySelector('.box');
+boxHTML.innerHTML = '<h2> content test <h2>'; // khác voi innerText chỉ insert duoc text vao content
+// innerHTML insert duoc ca HTML
+
+console.log(document.querySelector('.box h2').innerText);
+
+// DOM event
+// 1 inpyt / select
+// 2 keyup / down
+
+// sự kiện onchange dùng để kiểm tra điều kiện khi value trong input thay đổi\
+
+var inputelement = document.querySelector('input[type="text"]');
+
+inputelement.onchange = function(e) { // oninput
+    console.log(e.target.value);
+}
+
+// checked
+var inputelements = document.querySelector('input[type="checkbox"]');
+
+inputelements.onchange = function(e) { // oninput
+   var tesst = e.target.checked;
+   console.log(tesst);
+}
+
+// select
+
+var inputelements = document.querySelector('select');
+
+inputelements.onchange = function(e) { // oninput
+   var tesst = e.target.value;
+   console.log(tesst);
+}
