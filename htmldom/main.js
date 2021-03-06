37aca0d4024ae57a30d89bd6afe0b168ba1da8ff
thanhkma1996 <soldiersoociu@gmail.com>
2021-03-06 21:57:40 +0700

    6/3 preventdefault va stoppagation

--- htmldom/main.js
+++ htmldom/main.js
@@ -53,6 +53,24 @@ inputelement.onchange = function(e) { // oninput
     console.log(e.target.value);
 }
 
+// key up/down
+inputelement.onkeyup = function(e) { // oninput
+    // var tesst = e.target.value;
+    // console.log(tesst);
+        console.log(e.which); // casch nay dung de su dung keyboard xã dinh vi tri cua tu tren ban phim
+        switch(e.which) {
+            case 27 :
+                alert("EXIT");
+                break;
+            case 49 :
+                alert("Day la so 1 tren ban phim");
+                break;
+        }
+ }
+
+
+
+
 // checked
 var inputelements = document.querySelector('input[type="checkbox"]');
 
@@ -69,3 +87,36 @@ inputelements.onchange = function(e) { // oninput
    var tesst = e.target.value;
    console.log(tesst);
 }
+
+//  PreventDefault and StopPropagation
+// 1.PreventDefaul loại bỏ hành vi mặc định của element
+
+var aElements = document.links;
+
+// Kiem tra de chuyen link
+for(var i = 0; i < aElements.length; i++) {
+    aElements[i].onclick = function(e){
+        // console.log(e.target.href);
+        if(!e.target.href.startsWith('https://fullstack.edu.vn')){
+            e.preventDefault();
+        }
+    }
+}
+
+var Eveninput = document.querySelector('ul');
+
+Eveninput.onmousedown = function(e) { // hạnh vi onmousedown khi di chuột sẽ dùng e.prevendefault để ngăn chặn havnhf vi cũ
+    e.preventDefault();
+}
+
+// hành vi nổi bọt StopPropagation
+    document.querySelector('div.top-propagation').onclick = 
+        function(){
+            console.log('Day laf 1 DIV dang bi click');
+        }
+
+    document.querySelector('button').onclick = 
+        function(e){
+            e.stopPropagation(); // su dung thuoc tinh nay de khi click no chi tac dung den button duoc click k lam anh huong den button khac
+            console.log('Click me vao button');
+        }
