cd247860d4ec417b1e9ecf68a8550b7a5f2aa399
thanhkma1996 <soldiersoociu@gmail.com>
2021-03-21 22:30:38 +0700

    21/3 JSON Server , REST API , ussing POSTMAN

--- Json/app.js
+++ Json/app.js
@@ -9,3 +9,27 @@ var a = '1';
 console.log(JSON.parse(a));
 
  // Stringify chuyen du lieu cua js sang json
+
+ // Cài đặt JSON server : FAKE API server / Mock API
+//  https://github.com/typicode/json-server
+
+var couserAPI = "http://localhost:3000/posts";
+
+fetch(couserAPI)
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(post){
+        console.log(post);
+    })
+
+// Sử dụng POSTMAN làm việc với REST API
+// Fetch :
+    // + CRUD 
+    //     - Create: Tạo mới _> POST
+    //     - Read: Lấy dữ liệu -> GET
+    //     - Update: Chỉnh sửa -> PUT/PATCH
+    //     - Delete: Xóa -> DELETE
+
+// POSTMAN
